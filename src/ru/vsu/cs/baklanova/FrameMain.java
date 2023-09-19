121b009c9061e534f71e03835577933bb6445dc1
Sing Alex <dexp51477@gmail.com>
2023-09-19 18:50:46 +0300

    Make button for input

--- src/ru/vsu/cs/baklanova/FrameMain.java
+++ src/ru/vsu/cs/baklanova/FrameMain.java
@@ -1,24 +1,73 @@
 package ru.vsu.cs.baklanova;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class FrameMain extends JFrame{
     private JFrame Form;
-    private JPanel mainPanel;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
-    private JButton button1;
-    private JButton button2;
+    private JPanel panelMain;
+    private JTextArea textAreaInput;
+    private JTextArea textAreaOutput;
+    private JButton buttonSaveOutputIntoFile;
     private JButton button3;
-    private JButton button4;
+    private JButton buttonPrettyPrint;
+    private JButton buttonLoadInputFromFile;
+
+    private JFileChooser fileChooserOpen;
+    private JFileChooser fileChooserSave;
 
     public FrameMain() {
         this.setTitle("FrameMain");
-        this.setContentPane(mainPanel);
+        this.setContentPane(panelMain);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //this.setSize(1000, 800);
         this.pack();
 
+        fileChooserOpen = new JFileChooser();
+        fileChooserSave = new JFileChooser();
+        fileChooserOpen.setCurrentDirectory(new File("."));
+        fileChooserSave.setCurrentDirectory(new File("."));
+        FileFilter filter = new FileNameExtensionFilter("XML files", "xml");
+        fileChooserOpen.addChoosableFileFilter(filter);
+        fileChooserSave.addChoosableFileFilter(filter);
 
+        buttonLoadInputFromFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    if (fileChooserOpen.showOpenDialog(panelMain) == JFileChooser.APPROVE_OPTION) {
+                        String[] arr = Program.readLinesFromFile(fileChooserOpen.getSelectedFile().getPath());
+                        for (String s : arr) {
+                            textAreaInput.setText(textAreaInput.getText() + '\n' + s);
+                        }
+
+                    }
+                } catch (Exception e) {
+                    //SwingUtils.showErrorMessageBox(e);
+                }
+            }
+        });
+
+        buttonSaveOutputIntoFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    if (fileChooserSave.showSaveDialog(panelMain) == JFileChooser.APPROVE_OPTION) {
+                        //int[][] matrix = JTableUtils.readIntMatrixFromJTable(tableOutput);
+                        String file = fileChooserSave.getSelectedFile().getPath();
+                        if (!file.toLowerCase().endsWith(".txt")) {
+                            file += ".txt";
+                        }
+                        //ArrayUtils.writeArrayToFile(file, matrix);
+                    }
+                } catch (Exception e) {
+                    //SwingUtils.showErrorMessageBox(e);
+                }
+            }
+        });
     }
 }
